9240ed7e7e6dd516f2685c27cb4c47b7e77b9bd8
Zhi Qin <leezhiqinuk@gmail.com>
2024-10-24 09:32:08 +0800

    Using cin & cout

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,10 @@
 
 int main(){
     std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::cout << "Hello there, what's your name? " << std::flush;
+    // flush ensures output is completely sent to terminal before waiting for user input
+    std::cin >> str; // cin is only for single words
+    std::cout << "Nice to meet you, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
