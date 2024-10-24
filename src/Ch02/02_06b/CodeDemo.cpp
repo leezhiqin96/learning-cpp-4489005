a43c8a15c8e14c2266e4a1458dfbbb3c362d451f
Zhi Qin <leezhiqinuk@gmail.com>
2024-10-24 13:16:27 +0800

    Preprocessor directives

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,8 +3,23 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+// Preprocessor directives
+#define CAPACITY 5000
+#define DEBUG 
 
 int main(){
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+
+#ifdef DEBUG // if DEBUG is defined, then execute
+    std::cout << "[About to perform the addition" << std::endl;
+#endif
+
+    large += small;
+    std::cout << "The Large integer is " << large << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
