cb05059a871436cb111dc8a1a4d557c1ccb1cd3b
Zhi Qin <leezhiqinuk@gmail.com>
2024-10-24 13:30:49 +0800

    Declaring Arrays

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -5,6 +5,25 @@
 #include <iostream>
 
 int main(){
+    const size_t AGE_LENGTH = 4;
+    int age[AGE_LENGTH];
+    float temperature[] = {31.5, 32.7, 38.9};
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+
+    std::cout << "Age[0] = " << age[0] << std::endl;
+    std::cout << "Age[1] = " << age[1] << std::endl;
+    std::cout << "Age[2] = " << age[2] << std::endl;
+    std::cout << "Age[3] = " << age[3] << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Temperature[0] = " << temperature[0] << std::endl;
+    std::cout << "Temperature[1] = " << temperature[1] << std::endl;
+    std::cout << "Temperature[2] = " << temperature[2] << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
