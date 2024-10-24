13dea907ebecf6c5b99175b320aa5713cd5b41a6
Zhi Qin <leezhiqinuk@gmail.com>
2024-10-24 09:19:20 +0800

    Creating main function and printing hello

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hi There!" << std::endl; // endl -> new line
+    std::cout << std::endl;
+    return (0);
+}
