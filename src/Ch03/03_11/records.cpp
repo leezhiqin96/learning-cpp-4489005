dd7f107ccf09e53ecac9bd55d8378caa55e75653
Zhi Qin <leezhiqinuk@gmail.com>
2024-11-04 14:35:39 +0800

    Challenge - Create classes in implementation files

--- src/Ch03/03_11/records.cpp
+++ src/Ch03/03_11/records.cpp
@@ -1,1 +1,56 @@
 // Write your implementation code here
+#ifndef RECORD_H
+#define RECORD_H
+#include "records.h"
+
+Student::Student(int the_id, std::string the_name) {
+    id = the_id;
+    name = the_name;
+};
+
+int Student::get_id() const {
+    return id;
+};
+
+std::string Student::get_name() const {
+    return name;
+};
+
+Course::Course(int id_i, std::string name_i, unsigned char credits_i) {
+    id = id_i;
+    name = name_i;
+    credits = credits_i;
+}
+
+int Course::get_id() const {
+    return id;
+}
+
+std::string Course::get_name() const {
+    return name;
+}
+
+int Course::get_credits() const {
+    return credits;
+}
+
+Grade::Grade(int student_id_i, int course_id_i, char grade_i) {
+    student_id = student_id_i;
+    course_id = course_id_i;
+    grade = grade_i;
+}
+
+int Grade::get_student_id() const {
+    return student_id;
+}
+
+int Grade::get_course_id() const {
+    return course_id;
+}
+
+char Grade::get_grade() const {
+    return grade;
+}
+
+
+#endif //RECORD_H
